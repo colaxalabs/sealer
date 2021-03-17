e6b262dba2d74577fc7e002918876eec22d9e91c
3dw1nM0535 <workockmoses@gmail.com>
2021-03-17 14:57:13 +0300

    chore:test coverate reports and gas usage reporter setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,24 @@
+require('dotenv').config()
 import { HardhatUserConfig } from 'hardhat/config'
 
 import '@nomiclabs/hardhat-waffle'
+import 'solidity-coverage'
+import 'hardhat-gas-reporter'
 
 require('./scripts/deploy.ts')
 
 export default {
+  networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.ACCOUNT_KEYS]
+    },
+  },
   solidity: {
     version: "0.8.0",
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: process.env.REPORT_GAS ? true : false,
   },
 } as HardhatUserConfig
