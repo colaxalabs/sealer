68e2d8fb963bfc72633ea2f2a62c38a48eb70e9e
3dw1nM0535 <workockmoses@gmail.com>
2021-03-29 17:16:47 +0300

    chore(refactor):GSN capable contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,31 +1,52 @@
-require('dotenv').config()
-import { HardhatUserConfig } from 'hardhat/config'
+require("dotenv").config();
+import { HardhatUserConfig } from "hardhat/config";
 
-import '@nomiclabs/hardhat-waffle'
-import 'solidity-coverage'
-import 'hardhat-gas-reporter'
+import "@nomiclabs/hardhat-waffle";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
 
-require('./scripts/deploy.ts')
+require("./scripts/deploy.ts");
 
 export default {
   networks: {
+    buidlerevm: {
+      url: "http://localhost:8545",
+      gas: 1e8,
+      blockGasLimit: 1e8,
+    },
+    ganache: {
+      url: "http://localhost:8545",
+    },
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.ACCOUNT_KEYS]
+      accounts: [process.env.ACCOUNT_KEYS],
     },
   },
   solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: "0.7.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: "0.6.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   gasReporter: {
-    currency: 'KES',
+    currency: "KES",
     enabled: process.env.REPORT_GAS ? true : false,
     coinmarketcap: process.env.COIN_MARKET_API,
   },
-} as HardhatUserConfig
+} as HardhatUserConfig;
